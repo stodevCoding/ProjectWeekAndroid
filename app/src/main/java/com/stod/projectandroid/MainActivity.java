8255610e135147f99aba1cb0b77d087f8fb30bb5
Vlad <h.debelsunce@hotmail.fr>
2020-02-19 12:06:37 +0100

    visual accueil part1

--- app/src/main/java/com/stod/projectandroid/MainActivity.java
+++ app/src/main/java/com/stod/projectandroid/MainActivity.java
@@ -3,6 +3,10 @@ package com.stod.projectandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button demarrer = findViewById(R.id.demarrer);
+        final Button apropos = findViewById(R.id.apropos);
+        final ImageView pokeball = findViewById(R.id.pokeball);
+        final TextView title = findViewById(R.id.title);
+
+        demarrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        apropos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
