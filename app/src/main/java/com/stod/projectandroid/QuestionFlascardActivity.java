cb9ffd5888652b5d6369d5721427ee3ed6c1b82e
Felix <felix.litou@gmail.com>
2020-02-20 16:14:08 +0100

    Question counter

--- app/src/main/java/com/stod/projectandroid/QuestionFlascardActivity.java
+++ app/src/main/java/com/stod/projectandroid/QuestionFlascardActivity.java
@@ -13,11 +13,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class QuestionFlascardActivity extends AppCompatActivity {
     private Pokemon pokemon;
     private String goodAnswer;
     private String difficulty;
+    private int compteur;
     private int numQuestion;
 
     private String nom;
@@ -31,7 +33,12 @@ public class QuestionFlascardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_question_flascard);
 
         //pokemon = getIntent().getParcelableExtra("pokemon");
-
+        Intent intent = getIntent();
+        compteur = intent.getIntExtra("numQuestion",1);
+        numQuestion = compteur;
+        final TextView noQuestion = findViewById(R.id.noQuestionText);
+        noQuestion.setText("Question "+numQuestion);
+        compteur+=1;
         nom="bulbizare";
         imageId=R.drawable.bulbizarre;
 
@@ -41,9 +48,7 @@ public class QuestionFlascardActivity extends AppCompatActivity {
 
         final Button validate = findViewById(R.id.validateButton);
         final RadioGroup radioGroup = findViewById(R.id.answerRadioGroup);
-
-        Log.i(QuestionFlascardActivity.ACCESSIBILITY_SERVICE, "numero question : "+numQuestion+"");
-
+        
         validate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,6 +67,7 @@ public class QuestionFlascardActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     Intent intent2 = new Intent(QuestionFlascardActivity.this, QuestionFlascardActivity.class);
+                                    intent2.putExtra("numQuestion", compteur);
                                     startActivity(intent2);
                                     finish();
 
@@ -78,7 +84,7 @@ public class QuestionFlascardActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     Intent intent2 = new Intent(QuestionFlascardActivity.this, QuestionFlascardActivity.class);
-                                    intent2.putExtra("numQuestion", numQuestion); 
+                                    intent2.putExtra("numQuestion", compteur);
                                     startActivity(intent2);
                                     finish();
 
